b1efd29237e94365590e7850ee4e561470ad1c89
iraxus1 <piotrwiktor31@interia.pl>
2022-11-02 15:12:54 +0100

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -8,20 +8,23 @@ docker network rm test-net || true
 echo "Deploying app ($registry:$BUILD_NUMBER)..."
 docker network create test-net
 
-docker container run -d --name api --net test-net $registry:$BUILD_NUMBER
+docker container run -d \
+    --name api \
+    --net test-net \
+    $registry:$BUILD_NUMBER
 
 # Logic to wait for the api component to be ready on port 3000
 
-read -d '' wait_for << EOF
-echo "Waiting for API to listen on port 3000..."
-while ! nc -z api 3000; do
-  sleep 0.1 # wait for 1/10 of the second before check again
-  printf "."
-done
-echo "API ready on port 3000!"
+read -r -d '' CURL_COMMAND <<'EOF'
+curl -s -o /dev/null -w "%{http_code}" http://api:3000
 EOF
 
-docker container run --rm --net test-net node:alpine sh -c "$wait_for"
+docker container run --rm \
+    --net test-net \
+    node:alpine sh -c "$wait_for"
 
 echo "Smoke tests..."
-docker container run --name tester --rm --net test-net mmiotkug/node-curl sh -c "curl api:3000"
+docker container run --name tester \
+    --rm \
+    --net test-net \
+    mmiotkug/node-curl sh -c "curl api:3000"
