1a233401423238f6911f2d6d19329684416c843b
iraxus1 <piotrwiktor31@interia.pl>
2022-11-02 14:59:18 +0100

    chuj

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -3,28 +3,33 @@
 echo "Removing api container if it exists..."
 docker container rm -f api || true
 echo "Removing network test-net if it exists..."
-docker network rm test3-net || true
+docker network rm test-net || true
 
 echo "Deploying app ($registry:$BUILD_NUMBER)..."
-docker network create test3-net
+docker network create test-net
 
 docker container run -d \
     --name api \
-    --net test3-net \
+    --net test-net \
     $registry:$BUILD_NUMBER
 
 # Logic to wait for the api component to be ready on port 3000
 
-read -r -d '' CURL_COMMAND <<'EOF'
-curl -s -o /dev/null -w "%{http_code}" http://api:3000
+read -d '' wait_for << EOF
+echo "Waiting for API to listen on port 3000..."
+while ! nc -z api 3000; do
+  sleep 0.1 # wait for 1/10 of the second before check again
+  printf "."
+done
+echo "API ready on port 3000!"
 EOF
 
 docker container run --rm \
-    --net test3-net \
+    --net test-net \
     node:alpine sh -c "$wait_for"
 
 echo "Smoke tests..."
 docker container run --name tester \
     --rm \
-    --net test3-net \
+    --net test-net \
     mmiotkug/node-curl sh -c "curl api:3000"
