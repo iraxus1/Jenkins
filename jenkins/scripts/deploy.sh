7f35d88d2e99dac90fe834b603f837de8e0853a8
iraxus1 <piotrwiktor31@interia.pl>
2022-11-02 14:51:39 +0100

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -3,31 +3,28 @@
 echo "Removing api container if it exists..."
 docker container rm -f api || true
 echo "Removing network test-net if it exists..."
-docker network rm test-net || true
+docker network rm test3-net || true
 
 echo "Deploying app ($registry:$BUILD_NUMBER)..."
-docker network create test1-net
+docker network create test3-net
 
 docker container run -d \
     --name api \
-    --net test-net \
+    --net test3-net \
     $registry:$BUILD_NUMBER
 
 # Logic to wait for the api component to be ready on port 3000
 
 read -r -d '' CURL_COMMAND <<'EOF'
-curl --fail --silent --show-error \
-    --connect-timeout 1 \
-    --max-time 1 \
-    http://api:3000
+curl -s -o /dev/null -w "%{http_code}" http://api:3000
 EOF
 
 docker container run --rm \
-    --net test1-net \
+    --net test3-net \
     node:alpine sh -c "$wait_for"
 
 echo "Smoke tests..."
 docker container run --name tester \
     --rm \
-    --net test1-net \
+    --net test3-net \
     mmiotkug/node-curl sh -c "curl api:3000"
